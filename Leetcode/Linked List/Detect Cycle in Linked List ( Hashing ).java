10fd98320c82c99f19480686202ee8da2d04195c
KevzPeter <ultimatekevin1@gmail.com>
2021-09-07 02:35:52 +0530

    commit++

---
+++ Leetcode/Linked List/Detect Cycle in Linked List ( Hashing ).java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet <ListNode> s = new HashSet<>();
+        
+        
+        while(head!=null){
+            if(s.contains(head))
+                return true;
+            else 
+                s.add(head);
+            head=head.next;
+        }
+        return false;
+    }
+}
