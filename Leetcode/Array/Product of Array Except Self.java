9175b25c8215d9b9fdb909b5100ec97546aebd3f
KevzPeter <ultimatekevin1@gmail.com>
2021-11-30 23:05:16 +0530

    Update Product of Array Except Self.java

--- Leetcode/Array/Product of Array Except Self.java
+++ Leetcode/Array/Product of Array Except Self.java
@@ -13,4 +13,25 @@ class Solution {
         }
         return(ans);
     }
+    //O(n) solution
+    public int[] productExceptSelf(int[] nums) {
+        int ans[]=new int[nums.length];
+        int zeroes=0;
+        int product=1;
+        for( int i=0;i<nums.length;i++){
+            if(nums[i]==0) zeroes++;
+            else{
+                product*=nums[i];
+            }
+        }
+        for( int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                ans[i]=zeroes==1?product:0;
+            }
+            else{
+                ans[i]=zeroes>=1?0:product/nums[i];
+            }
+        }
+        return ans;
+    }
 }
