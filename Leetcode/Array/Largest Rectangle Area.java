95eb5348964ca742fc1dd0cbefdda103932bfbe5
KevzPeter <ultimatekevin1@gmail.com>
2021-11-30 23:08:21 +0530

    commit++

---
+++ Leetcode/Array/Largest Rectangle Area.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        
+        int max = 0;
+        int n = heights.length;
+        int leftHeight[] = new int[n];
+        int rightHeight[] = new int[n];
+        leftHeight[0] = -1;
+        rightHeight[n-1] = n;
+        
+        for( int i=1;i<n;i++){
+            int p=i-1;
+            while(p>=0 && heights[p]>=heights[i]){
+                p=leftHeight[p];
+            }
+            leftHeight[i]=p;
+        }
+        for( int i=n-1;i>=0;i--){
+            int p=i+1;
+            while(p<n && heights[p]>=heights[i]){
+                p=rightHeight[p];
+            }
+            rightHeight[i]=p;
+        }
+        for( int i=0;i<n;i++){
+            max=Math.max(max, heights[i]*(rightHeight[i]-leftHeight[i]-1));
+        }
+        return max;
+    }
+}
