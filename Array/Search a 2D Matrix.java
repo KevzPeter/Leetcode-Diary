9833eba955fe3374fc59e6190f12c21a337bd2e6
Kevin Peter <kekuriak@cisco.com>
2022-03-30 12:37:34 +0530

    daily

--- Array/Search a 2D Matrix.java
+++ Array/Search a 2D Matrix.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        ArrayList<Integer> col=new ArrayList<>();
-        for(int i=0;i<matrix.length;i++){
-            if(target>matrix[i][matrix[0].length-1])
-                continue;
-            else{
-               return bin_search(matrix[i], 0, matrix[0].length-1, target);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0; i<m; i++){
+            if(matrix[i][n-1] >= target){
+                return binSearch(matrix[i], target, 0, n-1);
             }
         }
         return false;
     }
-    boolean bin_search(int []arr, int start, int end, int target){
-        int mid = start +(end-start)/2;
-        while(start<=end){
-            if(arr[mid]==target)
-                return true;
-            if(arr[mid]>target)
-                return bin_search(arr, start, mid-1, target);
-            else
-                return bin_search(arr, mid+1, end, target);
+    private boolean binSearch(int[] arr, int target, int start, int end){
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target) return true;
+            else if(arr[mid] > target) end = mid - 1;
+            else start = mid + 1;
         }
         return false;
     }
