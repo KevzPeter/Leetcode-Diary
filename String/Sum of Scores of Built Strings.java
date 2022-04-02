69ecebd3544eaf028ba30ec131ad178a8c6c237f
KevzPeter <ultimatekevin1@gmail.com>
2022-04-03 02:08:37 +0530

    fixed typo

--- String/Sum of Scores of Built Strings.java
+++ String/Sum of Scores of Built Strings.java
@@ -1,32 +1,30 @@
 class Solution {
     public long sumScores(String s) {
-        if(s == null || s.length() == 0) return 0;
+        if (s == null || s.length() == 0)
+            return 0;
         int n = s.length();
         long[] zScore = new long[n];
         char[] input = s.toCharArray();
         int left = 0;
         int right = 0;
         zScore[0] = n;
-        for(int k = 1; k < input.length; k++) {
-            if(k > right) {
+        for (int k = 1; k < input.length; k++) {
+            if (k > right) {
                 left = right = k;
-                while(right < input.length && input[right] == input[right - left]) {
+                while (right < input.length && input[right] == input[right - left]) {
                     right++;
                 }
                 zScore[k] = right - left;
                 right--;
             } else {
-                //we are operating inside box
+                // we are operating inside box
                 int k1 = k - left;
-                //if value does not stretches till right bound then just copy it.
-                if(zScore[k1] < right - k + 1) {
+                // if value does not stretches till right bound then just copy it.
+                if (zScore[k1] < right - k + 1) {
                     zScore[k] = zScore[k1];
-                } else { //otherwise try to see if thepublic class Sum of Scores of Built Strings {
-                    
-                }
-                re are more matches.
+                } else { // otherwise try to see if there are more matches.
                     left = k;
-                    while(right < input.length && input[right] == input[right - left]) {
+                    while (right < input.length && input[right] == input[right - left]) {
                         right++;
                     }
                     zScore[k] = right - left;
@@ -34,12 +32,12 @@ class Solution {
                 }
             }
         }
-        
+
         long sumScore = 0;
-        for(long score : zScore){
+        for (long score : zScore) {
             sumScore += score;
         }
-        
+
         return sumScore;
     }
 }
