d2d1108678d5000a2caf1b6136ce16577cb3af5d
KevzPeter <ultimatekevin1@gmail.com>
2022-05-16 01:36:27 +0530

    deepest leaves sum

---
+++ Tree/Deepest Leaves Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        int res = 0, i;
+        LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            res = 0;
+            int sz = q.size();
+            for (i = 0; i < sz; i++) {
+                TreeNode node = q.poll();
+                res += node.val;
+                if (node.right != null)
+                    q.add(node.right);
+                if (node.left != null)
+                    q.add(node.left);
+            }
+        }
+        return res;
+    }
+}
