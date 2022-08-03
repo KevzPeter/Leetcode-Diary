fe505587a5433185daf1732ec726da1d302330ab
KevzPeter <ultimatekevin1@gmail.com>
2022-08-03 06:59:13 +0530

    Daily challenge

---
+++ HashMap/My Calendar I.java
@@ -0,0 +1,24 @@
+/*
+ * Using sorted list of intervals in TreeMap
+ * TC : O(NlogN)
+ * SC : O(N)
+ */
+
+class MyCalendar {
+    TreeMap<Integer, Integer> calendar;
+
+    MyCalendar() {
+        calendar = new TreeMap();
+    }
+
+    public boolean book(int start, int end) {
+        Integer prev = calendar.floorKey(start),
+                next = calendar.ceilingKey(start);
+        if ((prev == null || calendar.get(prev) <= start) &&
+                (next == null || end <= next)) {
+            calendar.put(start, end);
+            return true;
+        }
+        return false;
+    }
+}
