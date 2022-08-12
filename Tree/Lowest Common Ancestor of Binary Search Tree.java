56b2f9f9cc596fcb5a5876566b3af982ab851c85
KevzPeter <ultimatekevin1@gmail.com>
2022-08-12 12:02:58 +0530

    daily

---
+++ Tree/Lowest Common Ancestor of Binary Search Tree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (p.val < root.val && q.val < root.val)
+                root = root.left;
+            else if (p.val > root.val && q.val > root.val)
+                root = root.right;
+            else
+                return root;
+        }
+    }
+}
