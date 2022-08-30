8001610b72cef73ba48b62e9a15cbed664afe3a5
KevzPeter <ultimatekevin1@gmail.com>
2022-08-30 20:23:18 +0530

    another approach added

--- Array/Rotate Image.java
+++ Array/Rotate Image.java
@@ -1,18 +1,34 @@
 class Solution {
     public void rotate(int[][] matrix) {
-    
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<i;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        for( int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length/2;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][matrix[0].length-1-j];
-                matrix[i][matrix[0].length-1-j]=temp;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
+                matrix[i][matrix[0].length - 1 - j] = temp;
+            }
+        }
+    }
+}
+
+// Approach #2
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
             }
         }
     }
