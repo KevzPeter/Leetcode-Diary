b4e7e293acaa8c26624c7f83adcd57da42987531
KevzPeter <ultimatekevin1@gmail.com>
2022-09-14 21:49:10 +0530

    added solutions using bitwise operations

--- Tree/Pseudo Palindromic Paths in Binary Tree.java
+++ Tree/Pseudo Palindromic Paths in Binary Tree.java
@@ -13,7 +13,43 @@
  * }
  * }
  */
-class Solution {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+// using bitwise operations to keep track of frequency of values
+class Solution1 {
+    public int pseudoPalindromicPaths(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int path) {
+        int ans = 0;
+        path ^= (1 << root.val);
+        if (root.left == null && root.right == null) {
+            ans += (path & (path - 1)) == 0 ? 1 : 0;
+        }
+        if (root.left != null)
+            ans += dfs(root.left, path);
+        if (root.right != null)
+            ans += dfs(root.right, path);
+        return ans;
+    }
+}
+
+// using arraylist to keep track of frequency of values
+class Solution2 {
     public int pseudoPalindromicPaths(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
