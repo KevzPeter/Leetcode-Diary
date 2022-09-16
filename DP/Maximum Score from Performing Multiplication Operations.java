8e58eb919c8f4d08effd3ab61f1350af4ef953da
KevzPeter <ultimatekevin1@gmail.com>
2022-09-16 21:12:25 +0530

    added 1D DP solution

--- DP/Maximum Score from Performing Multiplication Operations.java
+++ DP/Maximum Score from Performing Multiplication Operations.java
@@ -71,3 +71,25 @@ class Solution {
         return dp[0][0];
     }
 }
+
+// Space optimized 1D DP (only solution that doesn't result in TLE)
+class Solution {
+    public int maximumScore(int[] nums, int[] multipliers) {
+        // For Right Pointer
+        int n = nums.length;
+        // Number of Operations
+        int m = multipliers.length;
+        int[] dp = new int[m + 1];
+
+        for (int op = m - 1; op >= 0; op--) {
+            int[] next_row = dp.clone();
+            // Present Row is now next_Row because we are moving upwards
+            for (int left = op; left >= 0; left--) {
+                dp[left] = Math.max(multipliers[op] * nums[left] + next_row[left + 1],
+                        multipliers[op] * nums[n - 1 - (op - left)] + next_row[left]);
+            }
+        }
+
+        return dp[0];
+    }
+}
