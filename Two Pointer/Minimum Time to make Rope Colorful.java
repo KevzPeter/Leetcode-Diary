41ad5d209a9f1085374f90ce93d2fde5ab14760c
KevzPeter <ultimatekevin1@gmail.com>
2022-10-03 22:44:00 +0530

    daily

---
+++ Two Pointer/Minimum Time to make Rope Colorful.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int minCost = 0;
+        int start = 0, end = 1;
+        int n = colors.length();
+        while(end < n){
+            if(colors.charAt(end) == colors.charAt(start)){
+                if(neededTime[start] < neededTime[end]){
+                    minCost += neededTime[start];
+                    start = end;
+                }
+                else{
+                    minCost += neededTime[end];
+                }
+            }
+            else{
+                start = end;
+            }
+            end = end + 1;
+        }
+        return minCost;
+    }
+}
