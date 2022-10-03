756654b5108eff57aa68a8ba0d32c859f4057e94
KevzPeter <ultimatekevin1@gmail.com>
2022-10-03 22:45:07 +0530

    added tc sc

--- Two Pointer/Minimum Time to make Rope Colorful.java
+++ Two Pointer/Minimum Time to make Rope Colorful.java
@@ -1,19 +1,22 @@
+/*
+ * Daily challenge: 3/10/22
+ * TC : O(n)
+ * SC : O(1)
+ */
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int minCost = 0;
         int start = 0, end = 1;
         int n = colors.length();
-        while(end < n){
-            if(colors.charAt(end) == colors.charAt(start)){
-                if(neededTime[start] < neededTime[end]){
+        while (end < n) {
+            if (colors.charAt(end) == colors.charAt(start)) {
+                if (neededTime[start] < neededTime[end]) {
                     minCost += neededTime[start];
                     start = end;
-                }
-                else{
+                } else {
                     minCost += neededTime[end];
                 }
-            }
-            else{
+            } else {
                 start = end;
             }
             end = end + 1;
