cbf86b82eb0737959c5ccbc7417ed6a1b9a68262
KevzPeter <ultimatekevin1@gmail.com>
2022-11-20 07:37:36 +0530

    updated to one loop

--- Template/ConvexHull.java
+++ Template/ConvexHull.java
@@ -4,25 +4,34 @@ public class Solution {
     }
 
     public int[][] outerTrees(int[][] points) {
+
+        final int n = points.length;
         Arrays.sort(points, new Comparator<int[]>() {
             public int compare(int[] p, int[] q) {
                 return q[0] - p[0] == 0 ? q[1] - p[1] : q[0] - p[0];
             }
         });
-        Stack<int[]> hull = new Stack<>();
+        Stack<int[]> upperHull = new Stack<>();
+        Stack<int[]> lowerHull = new Stack<>();
         for (int i = 0; i < points.length; i++) {
-            while (hull.size() >= 2 && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), points[i]) > 0)
-                hull.pop();
-            hull.push(points[i]);
-        }
-        hull.pop();
-        for (int i = points.length - 1; i >= 0; i--) {
-            while (hull.size() >= 2 && orientation(hull.get(hull.size() - 2), hull.get(hull.size() - 1), points[i]) > 0)
-                hull.pop();
-            hull.push(points[i]);
+            int uSize = upperHull.size();
+            int lSize = lowerHull.size();
+            while (uSize >= 2 && orientation(upperHull.get(uSize - 2), upperHull.get(uSize - 1), points[i]) > 0) {
+                --uSize;
+                upperHull.pop();
+            }
+            upperHull.push(points[i]);
+            while (lSize >= 2
+                    && orientation(lowerHull.get(lSize - 2), lowerHull.get(lSize - 1), points[n - i - 1]) > 0) {
+                --lSize;
+                lowerHull.pop();
+            }
+            lowerHull.push(points[n - i - 1]);
         }
         // remove redundant elements from the stack
-        HashSet<int[]> ret = new HashSet<>(hull);
+        HashSet<int[]> ret = new HashSet<>();
+        ret.addAll(upperHull);
+        ret.addAll(lowerHull);
         return ret.toArray(new int[ret.size()][]);
     }
 }
