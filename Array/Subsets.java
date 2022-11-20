ae9fdda24e4613bf4d2b44ece2046c55d4783102
KevzPeter <ultimatekevin1@gmail.com>
2022-11-20 07:16:20 +0530

    minor changes

--- Array/Subsets.java
+++ Array/Subsets.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<List<Integer>>();
-        for(int i=0;i<Math.pow(2, nums.length);i++){
-            List<Integer> ds=new ArrayList<>();
-            for (int j=0;j<nums.length;j++){
-                if((i & 1<<j)!=0){
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        for (int i = 0; i < Math.pow(2, nums.length); i++) {
+            List<Integer> ds = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++) {
+                if ((i & 1 << j) != 0) {
                     ds.add(nums[j]);
                 }
             }
