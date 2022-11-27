78ddab04b1af00afab8a68014e1341dcf9a39e8d
KevzPeter <ultimatekevin1@gmail.com>
2022-11-27 20:47:36 +0530

    WC 321 Q3 LL prob solved using MonoStack

---
+++ Stack/Remove Nodes from LinkedList.java
@@ -0,0 +1,22 @@
+// Solution using Monotonic stack
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+        while (temp != null) {
+            int val = temp.val;
+            while (!stack.isEmpty() && stack.peek() < val)
+                stack.pop();
+            stack.push(val);
+            temp = temp.next;
+        }
+        temp = new ListNode();
+        head = temp;
+        Iterator it = stack.iterator();
+        for (Integer i : stack) {
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+        return head.next;
+    }
+}
