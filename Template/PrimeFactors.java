baba32daec32f12ead414862e92d1d655390f831
KevzPeter <ultimatekevin1@gmail.com>
2023-01-01 08:24:12 +0530

    small typo cost me 5 min penalty in LC contest :(

--- Template/PrimeFactors.java
+++ Template/PrimeFactors.java
@@ -8,7 +8,7 @@ public class PrimeFactors {
             factors.add(2);
             n /= 2;
         }
-        for (int i = 3; i * i < n; i += 2) {
+        for (int i = 3; i * i <= n; i += 2) {
             while (n % i == 0) {
                 factors.add(i);
                 n /= i;
