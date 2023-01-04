a0bbc714c23a1b14da77872fb870a659d80f706d
KevzPeter <ultimatekevin1@gmail.com>
2023-01-04 07:19:10 +0530

    optimized java code

--- HashMap/Minimum Rounds to Complete all Tasks.java
+++ HashMap/Minimum Rounds to Complete all Tasks.java
@@ -2,12 +2,12 @@ class Solution {
     public int minimumRounds(int[] tasks) {
         int rounds = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int task : tasks){
+        for (int task : tasks) {
             map.put(task, map.getOrDefault(task, 0) + 1);
         }
-        for(Map.Entry<Integer, Integer> e : map.entrySet()){
-            int freq = e.getValue();
-            if(freq <= 1) return -1;
+        for (Integer freq : map.values()) {
+            if (freq == 1)
+                return -1;
             rounds += freq % 3 == 0 ? freq / 3 : freq / 3 + 1;
         }
         return rounds;
