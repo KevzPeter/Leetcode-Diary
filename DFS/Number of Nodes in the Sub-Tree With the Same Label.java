cb6dff5e7d97458007a16a04cca9ce33118d1307
KevzPeter <ultimatekevin1@gmail.com>
2023-01-12 06:50:19 +0530

    minor change

--- DFS/Number of Nodes in the Sub-Tree With the Same Label.java
+++ DFS/Number of Nodes in the Sub-Tree With the Same Label.java
@@ -9,25 +9,24 @@ class Solution {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-        dfs(0, adj, labels, ans, new int[26], new boolean[n]);
+        dfs(0, -1, adj, labels, ans, new int[26]);
         return ans;
     }
 
-    private int[] dfs(int curr, List<List<Integer>> adj, String labels, int[] ans, int[] count,
-            boolean[] vis) {
-        vis[curr] = true;
-        char currLabel = labels.charAt(curr);
+    private int[] dfs(int currNode, int parent, List<List<Integer>> adj, String labels, int[] ans, int[] count) {
+        char currLabel = labels.charAt(currNode);
         count[currLabel - 'a']++;
-        for (int nei : adj.get(curr)) {
-            if (!vis[nei]) {
+        for (int child : adj.get(currNode)) {
+            if(child == parent) continue;
+            else{
                 int[] countChild = new int[26];
-                int[] res = dfs(nei, adj, labels, ans, countChild, vis);
+                int[] res = dfs(child, currNode, adj, labels, ans, countChild);
                 for (int i = 0; i < 26; i++) {
                     count[i] += res[i];
                 }
             }
         }
-        ans[curr] = count[currLabel - 'a'];
+        ans[currNode] = count[currLabel - 'a'];
         return count;
     }
 }
