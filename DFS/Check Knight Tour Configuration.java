9d24493a38018ccdc475e8ec0bd3cdb5c25d4eac
KevzPeter <ultimatekevin1@gmail.com>
2023-03-20 07:00:39 +0530

    Check Knight Tour Configuration

---
+++ DFS/Check Knight Tour Configuration.java
@@ -0,0 +1,26 @@
+class Solution {
+    private static final int[][] MOVES = {{1, 2}, {-1, 2}, {1, -2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
+    public boolean checkValidGrid(int[][] grid) {
+        int n = grid.length;
+        int i = 0, j = 0;
+        for(int currMove = 0; currMove < (n * n) - 1; currMove++){
+            int newPosI = i, newPosJ = j;
+            outer: for(int[] move : MOVES){
+                int x = i + move[0];
+                int y = j + move[1];
+                if(isValid(x, y, n, n) && grid[x][y] == currMove + 1){
+                    newPosI = x;
+                    newPosJ = y;
+                    continue outer;
+                }
+            }
+            if(i == newPosI && j == newPosJ) return false;
+            i = newPosI;
+            j = newPosJ;
+        }
+        return true;
+    }
+    private boolean isValid(int i, int j, int m, int n){
+        return (i >= 0 && i < m && j >= 0 && j < n);
+    }
+}
