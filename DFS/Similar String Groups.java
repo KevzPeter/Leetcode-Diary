366a771c300d5d263c92b47617c4cd88a5e739c5
KevzPeter <ultimatekevin1@gmail.com>
2023-04-29 06:58:30 +0530

    Added problem description

--- DFS/Similar String Groups.java
+++ DFS/Similar String Groups.java
@@ -1,22 +1,38 @@
+/**
+ * Two strings X and Y are similar if we can swap two letters (in different
+ * positions) of X, so that it equals Y. Also two strings X and Y are similar if
+ * they are equal.
+ * For example, "tars" and "rats" are similar (swapping at positions 0 and 2),
+ * and "rats" and "arts" are similar, but "star" is not similar to "tars",
+ * "rats", or "arts".
+ * Together, these form two connected groups by similarity: {"tars", "rats",
+ * "arts"} and {"star"}. Notice that "tars" and "arts" are in the same group
+ * even though they are not similar. Formally, each group is such that a word is
+ * in the group if and only if it is similar to at least one other word in the
+ * group.
+ * We are given a list strs of strings where every string in strs is an anagram
+ * of every other string in strs. How many groups are there?
+ */
 class Solution {
     public int numSimilarGroups(String[] strs) {
         List<List<Integer>> adj = new ArrayList<>();
         int len = strs.length;
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             char[] currWord = strs[i].toCharArray();
-            for(int j = i + 1; j < len; j++){
+            for (int j = i + 1; j < len; j++) {
                 char[] nextWord = strs[j].toCharArray();
                 int notMatch = 0;
-                if(currWord.length != nextWord.length) continue;
-                for(int k = 0; k < currWord.length; k++){
-                    if(currWord[k] != nextWord[k]){
+                if (currWord.length != nextWord.length)
+                    continue;
+                for (int k = 0; k < currWord.length; k++) {
+                    if (currWord[k] != nextWord[k]) {
                         notMatch++;
                     }
                 }
-                if(notMatch <= 2){
+                if (notMatch <= 2) {
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
@@ -24,18 +40,19 @@ class Solution {
         }
         int similarGroups = 0;
         boolean[] visited = new boolean[len];
-        for(int i = 0; i < len; i++){
-            if(!visited[i]){
+        for (int i = 0; i < len; i++) {
+            if (!visited[i]) {
                 ++similarGroups;
                 dfs(i, adj, visited);
             }
         }
         return similarGroups;
     }
-    private void dfs(int curr, List<List<Integer>> adj, boolean[] visited){
+
+    private void dfs(int curr, List<List<Integer>> adj, boolean[] visited) {
         visited[curr] = true;
-        for(int nei : adj.get(curr)){
-            if(!visited[nei]){
+        for (int nei : adj.get(curr)) {
+            if (!visited[nei]) {
                 dfs(nei, adj, visited);
             }
         }
